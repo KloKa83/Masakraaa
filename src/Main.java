561ebf792e2ced7354353495e4ff81b295217b15
KloKa83 <klosins.ka.tarzyna@gmail.com>
2022-10-24 20:23:03 +0200

    zmiana imienia

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String name="Kasia";
+        System.out.println("hello"+name);
+
     }
 }
